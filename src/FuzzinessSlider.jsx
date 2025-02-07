2e07b8e7cde275c5d83b3d62dc5196e78839a01e
Tom Rosenbloom <TomRosenbloom@users.noreply.github.com>
2025-02-07 16:48:24 +0000

    moved fuzziness slider into separate component

--- src/FuzzinessSlider.jsx
+++ src/FuzzinessSlider.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function FuzzinessSlider(value, onChange) {
+export default function FuzzinessSlider({value, onChange}) {
 
   return (
     <input
@@ -14,33 +14,3 @@ export default function FuzzinessSlider(value, onChange) {
   )
 
 }
-
-
-
-
-
-  
-  
-  export default function Dropdown({options, onSelect}) {
-    const [selected, setSelected] = useState("");
-  
-    const handleChange = (event) => {
-      const value = event.target.value;
-      setSelected(value);
-      onSelect(value); // Pass selected value to parent
-    };
-  
-    return (
-      <div>
-      <select value={selected} onChange={handleChange}>
-        <option value="" disabled>Select an option</option>
-        {options.map((option, index) => (
-          <option key={index} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-  
-      </div>
-    )
-  }
